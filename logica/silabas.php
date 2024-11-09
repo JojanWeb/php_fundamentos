f330c600bc5fe93f74b5141ffc1a376cf6e5e0d2
Johan Sebastian Delgado Mantilla <zzaibor69@gmail.com>
2024-11-08 22:00:29 -0500

    n

--- logica/silabas.php
+++ logica/silabas.php
@@ -2,7 +2,7 @@
 
 // crea una funcion para calcular cuantas silabas tiene una palabra
 
-$texto = "!";
+$texto = "AaDsSoO1a345";
 
 // $expresion = "/PRUEBA/i";
 // $expresion = "/^PRUEBA/i";
@@ -10,7 +10,22 @@ $texto = "!";
 // $expresion = "/[0-9]/";
 // $expresion = "/[\d]/";
 // $expresion = "/[A-Z]{4}/";  
-$expresion = "/./";  
+// $expresion = "/./";  
+
+// $RESULTADO = preg_match_all($expresion, $texto,$coincidencias, PREG_OFFSET_CAPTURE);
+
+// if ($RESULTADO){
+//   echo "Si tiene resultado";
+// }else{
+//   echo "NO tiene resultado";
+// }
+
+// echo "<pre>";
+// print_r($coincidencias);
+// echo "</pre>";
+
+// $expresion = "/(?=(?:.*[A-Z]){4})(?=(?:.*\d){4})/";  
+$expresion = "/((?:.*?[A-Z]){4}).*((?:.*?\d){4})/";  
 
 $RESULTADO = preg_match_all($expresion, $texto,$coincidencias, PREG_OFFSET_CAPTURE);
 
@@ -25,6 +40,11 @@ print_r($coincidencias);
 echo "</pre>";
 
 
+// if (preg_match('/^(?=(?:.*[A-Z]){4})(?=(?:.*\d){4})/', $texto)) {
+//     echo "El texto tiene al menos 4 letras mayúsculas y 4 números.";
+// } else {
+//     echo "El texto no cumple con los requisitos.";
+// }
 
 
 
